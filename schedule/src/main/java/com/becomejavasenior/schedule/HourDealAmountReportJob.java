ee13c200ab4ccf0866c853f95afd83d3632469cb
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-09-10 22:01:52 +0300

    module tasks renamed to schedule

---
+++ schedule/src/main/java/com/becomejavasenior/schedule/HourDealAmountReportJob.java
@@ -0,0 +1,24 @@
+package com.becomejavasenior.schedule;
+
+import com.becomejavasenior.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HourDealAmountReportJob  {
+    @Autowired
+    ReportService reportService;
+
+    @Scheduled(cron = "0 0 0/1 1/1 * ?")
+        protected void executeInternal() {
+        reportService.makeHourDealAmountReports();
+        System.out.println("Hour Company Deal Amount Report is done.");
+    }
+
+    /*protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        reportService.makeHourDealAmountReports();
+        System.out.println("Hour Company Deal Amount Report is done.");
+    }*/
+
+}
